037f107e1d94acfa2031159e99cd668009e831df
ethanjtcs <ethanj-farmington@thecoderschool.com>
2024-10-15 22:47:22 +0000

    its a basic cauculator that has basic commands and some changes

--- calculator.py
+++ calculator.py
@@ -1,7 +1,13 @@
 num1 = int(input("put your fav number"))
 num2 = int(input("put your second fav number"))
 op = input("+,-,*,/ wut are you gonna put")
-print(num1, op num2)
+print(num1, op, num2)
 
-        
-
+if op == "+":       
+    print(num1 + num2)   
+elif op == "-":       
+    print(num1 - num2)   
+elif op == "*":       
+    print(num1 * num2)   
+elif op == "/":       
+    print(num1 / num2)   
